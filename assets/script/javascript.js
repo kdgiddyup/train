a75dde0f50bb216ac2d0d3931059fdcb297d6265
Kelly Davis <kellydavis1974@gmail.com>
2017-03-17 21:17:26 -0400

    added auto-updating function and associated variables - localSnapshot array to store firstTime and frequency info to calculate new times based on moment() objects

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -8,7 +8,8 @@ messagingSenderId: "539527111260"
 };
 firebase.initializeApp(config);
 var db = firebase.database();
-
+// localSnapshot array holds train objects to aid in auto-updating times
+localSnapshot=[];
  
 // function addTrain adds train row to HTML
 function addTrain(newTrain) {
@@ -19,8 +20,7 @@ function addTrain(newTrain) {
 	var tRemainder = diffTime % tFrequency;
 	var tMinutesTillTrain = tFrequency - tRemainder;
 	var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-
-	$("#trainTable").children("tbody").append("<tr><td>"+newTrain.name+"</td><td>"+newTrain.destination+"</td><td>"+newTrain.frequency+"</td><td>"+tMinutesTillTrain+"</td><td>"+moment(nextTrain).format("hh:mm")+"</td></tr>");
+	$("#trainTable").children("tbody").append("<tr><td>"+newTrain.name+"</td><td>"+newTrain.destination+"</td><td>"+newTrain.frequency+"</td><td>"+moment().add(tMinutesTillTrain,"minutes").format("hh:mm a")+"</td><td>"+tMinutesTillTrain+"</td></tr>");
 }
 
 $(document).ready(function(){
@@ -35,6 +35,9 @@ $("#trainSubmit").on("click",function(event){
 }); // end submit function	
 	
 function formSubmit(){
+	console.log('checking times');
+	// don't continue to add new intervals with each train addition
+	clearInterval(autoCheck);
 	var inputs = $("input");
 
 	// we'll check inputs for empties
@@ -57,7 +60,6 @@ function formSubmit(){
 			firstTime:inputs[2].value,
 			frequency:inputs[3].value
 		};
-		console.log(thisTrain);
 		db.ref().push(thisTrain);
 		// now empty the inputs and restore non-warning state/placeholders
 		$("#trainInput").val('').attr("placeholder","Train Name").removeClass("empty");
@@ -66,25 +68,38 @@ function formSubmit(){
 		$("#frequencyInput").val('').attr("placeholder","Time in minutes").removeClass("empty");
 	} 
 }
-
+// function to auto-update time calculations in table
+function updateTimes(){
+	// loop through table rows, find time elements and update them based on current time
+	$("tbody").children("tr").each(function(index){
+		var uFrequency = localSnapshot[index].frequency;
+		// string in 24 hour format, subtracted 1 year
+		var firstTime = moment(localSnapshot[index].firstTime, "H HH").subtract(1,"years");
+		// minutes from first train time to now, in minutes 
+		var diffTime = moment().diff(moment(firstTime), "minutes");
+		// total minutes modulo frequency gives you time until next train
+		var uRemainder = diffTime % uFrequency;
+		// frequency minus time to next train is minutes to next train
+		var uMinutesTillTrain = uFrequency - uRemainder;
+		// next train time is time now + minutes to next train
+		var nextTrain = moment().add(uMinutesTillTrain, "minutes").format("hh:mm a");
+		// update cells with new calculations
+		$(this).children("td").eq(3).text(nextTrain);
+		$(this).children("td").eq(4).text(uMinutesTillTrain);
+	});
+}
 // this is the first code executed
 // grab existing data from firebase
 // when doc loads, listen for all children (if any) in firebase and sends .val() to addTrain function to build table
 
 db.ref().on('child_added',function(childSnapshot){
+	// create local version of db for auto-update purposes (need frequency, first time data)
+	localSnapshot.push(childSnapshot.val());
 	addTrain(childSnapshot.val());
 	});	
 
+//create interval to auto-update train times once per minute
+var autoCheck = setInterval(updateTimes,10000);
 
 }) // end ready function
 
-/* 
-snapshot:
-
-train name: {
-	destination: ,
-	firstTime: ,
-	frequency: 		
-} 
-
-*/
